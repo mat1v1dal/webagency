f923d622458744868960e71117e245a9d91e6b9a
mat1v1dal <2302403@ucc.edu.ar>
2025-02-11 12:37:19 -0300

    hero update

--- src/components/Hero3d.jsx
+++ src/components/Hero3d.jsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/8fZ8Odw1sofoQFRO/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/RoDJHb7EZzBDwBzS/scene.splinecode" />
   );
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,10 +25,14 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 z-10">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hola, somos NoaCode <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Construimos soluciones digitales a medida</p>
       </div>
 
-      <div className="w-full h-full absolute inset-0">
+      <div className="w-full h-full mt-20 absolute inset-0">
         <Hero3d />
       </div>
 
