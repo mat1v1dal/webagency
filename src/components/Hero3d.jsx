c18834a85101edd1bf4a78c261e61fcd11313e79
mat1v1dal <2302403@ucc.edu.ar>
2025-02-11 12:46:40 -0300

    hero update

--- src/components/Hero3d.jsx
+++ src/components/Hero3d.jsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/RoDJHb7EZzBDwBzS/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/YuHkT-kqL1047iun/scene.splinecode" />
   );
 }
